f446b59c67419e272ae00809844f226c9066b362
romildo <malaquias@gmail.com>
2016-10-07 11:03:05 -0300

    Support command line options

--- src/main/java/main/Driver.java
+++ src/main/java/main/Driver.java
@@ -1,23 +1,90 @@
 package main;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import java_cup.runtime.Symbol;
-
 import parse.Yylex;
 import parse.sym;
 
+// command line options
+class DriverOptions {
+   @Parameter(description = "<source file>")
+   public List<String> parameters = new ArrayList<>();
+
+   @Parameter(names = {"--help", "-h"}, description = "Usage help", help = true)
+   public boolean help = false;
+
+   @Parameter(names = {"--lexer"}, description = "Lexical analysis")
+   public boolean lexer = true;
+}
+
+// main
 public class Driver {
 
-  public static void main(String[] args) throws Exception {
-    Reader input = new InputStreamReader(System.in);
-    Yylex lexer = new Yylex(input);
-    Symbol tok;
-    do {
-      tok = lexer.next_token();
-      System.out.println(tok);
-    } while (tok.sym != sym.EOF);
-  }
+   public static void main(String[] args) {
+      // parse command line options
+      DriverOptions options = new DriverOptions();
+      JCommander jCommander = new JCommander(options);
+      jCommander.setProgramName("Driver");
+
+      try {
+         jCommander.parse(args);
+      }
+      catch (ParameterException e) {
+         System.out.println(e.getMessage());
+         jCommander.usage();
+         System.exit(1);
+      }
+
+      if (options.help) {
+         jCommander.usage();
+         return;
+      }
+
+      Reader input = null;
+      try {
+         // set the input (source code) to compile
+         if (options.parameters.isEmpty())
+            input = new InputStreamReader(System.in);
+         else
+            input = new FileReader(options.parameters.get(0));
+
+         // do only lexical analyses
+         if (options.lexer)
+            lexicalAnalysis(input);
+      }
+      catch (IOException e) {
+         System.out.println(e.getMessage());
+         System.exit(2);
+      }
+      finally {
+         // closes the input file
+         if (input instanceof FileReader)
+            try {
+               input.close();
+            }
+            catch (IOException e) {
+               System.out.println(e.getMessage());
+               System.exit(3);
+            }
+      }
+   }
+
+   public static void lexicalAnalysis(Reader input) throws IOException {
+      Yylex lexer = new Yylex(input);
+      Symbol tok;
+      do {
+         tok = lexer.next_token();
+         System.out.println(tok);
+      } while (tok.sym != sym.EOF);
+   }
 
 }
