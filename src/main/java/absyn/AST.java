c73356d484e7e625304a9893d525290ce19b2d50
romildo <malaquias@gmail.com>
2016-10-09 14:06:13 -0300

    Overload toString method in AST class using commons-lang3 library

--- src/main/java/absyn/AST.java
+++ src/main/java/absyn/AST.java
@@ -1,5 +1,13 @@
 package absyn;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public abstract class AST {
+
+   @Override
+   public String toString() {
+      return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+   }
 
 }
