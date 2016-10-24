0f7851a7fbfd341122145c156864bd3424042a91
romildo <malaquias@gmail.com>
2016-10-24 07:10:18 -0200

    Rename class 'ExpNum' to 'ExpReal'

--- src/main/java/absyn/ExpReal.java
+++ src/main/java/absyn/ExpReal.java
@@ -7,27 +7,27 @@ import types.Type;
 
 import static org.bytedeco.javacpp.LLVM.*;
 
-public class ExpNum extends Exp {
+public class ExpReal extends Exp {
 
    public final Double value;
 
-   public ExpNum(Loc loc, Double value) {
+   public ExpReal(Loc loc, Double value) {
       super(loc);
       this.value = value;
    }
 
    @Override
    public Tree.Node<String> toTree() {
-      return Tree.of(annotateType("ExpNum: " + value));
-   }
-
-   @Override
-   public LLVMValueRef codegen(LLVMModuleRef module, LLVMBuilderRef builder) {
-      return LLVMConstReal(LLVMDoubleType(), value);
+      return Tree.of(annotateType("ExpReal: " + value));
    }
 
    @Override
    protected Type semantic_() {
       return REAL.T;
+   }
+
+   @Override
+   public LLVMValueRef codegen(LLVMModuleRef module, LLVMBuilderRef builder) {
+      return LLVMConstReal(LLVMDoubleType(), value);
    }
 }
