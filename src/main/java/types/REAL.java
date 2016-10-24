37a7a71f248aa79d3ee795d6d541ef99d079390b
romildo <malaquias@gmail.com>
2016-10-24 00:19:33 -0200

    Add type package and type representation for 'real'

---
+++ src/main/java/types/REAL.java
@@ -0,0 +1,14 @@
+package types;
+
+public class REAL extends Type {
+
+   public static final REAL T = new REAL();
+
+   private REAL() {
+   }
+
+   @Override
+   public String toString() {
+      return "real";
+   }
+}
